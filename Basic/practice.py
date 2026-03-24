59de66693520a1bc108a64863ef4376c1ebf1582
sitesh-dotcom <siteshmail@gmail.com>
2026-03-24 18:26:12 +0000

    file modified

--- Basic/practice.py
+++ Basic/practice.py
@@ -1,7 +1,27 @@
 # num1 = 100
 # num2 = 200
-num1,num2 = 100,200
-add = num1 + num2
+# num1,num2 = 100,200
 num1 = num2 = 200
+add = num1 + num2
 print("addition ", add)
+print(type (add))
+msg = """
+ml = machne learning
+dl = deep learning
+NLP = natural language processing
+Gen AI = Generative AI
+"""
+print(msg)
+print(type(msg))
+flag1 = True
+flag2 = False
+print(flag1)
+print(flag2)
+print(type(flag1))
+age = 20
+flag1 = age > 18
+print(flag1)
+numbers = [100,200,300,400,500]
+print(numbers[0],numbers[-5])
+
 
